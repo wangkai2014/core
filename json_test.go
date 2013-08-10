89e116b0273b5cc8b47d278118d65603b9e9dd58
Christopher Jackson <himself@cj-jackson.com>
2013-08-10 18:41:47 +0100

    Added test for json and xml!

--- json_test.go
+++ json_test.go
@@ -1,5 +1,13 @@
 package core
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
 func ExampleCore_Json(c *Core) {
 	// Prepare structure
 	data := struct {
@@ -12,4 +20,49 @@ func ExampleCore_Json(c *Core) {
 
 	// Send it back to the client
 	c.Json().Send(data)
+}
+
+func TestJson(t *testing.T) {
+	type jsonTest struct {
+		Title string `json:"title"`
+	}
+
+	App := NewApp()
+
+	App.Debug = true
+
+	App.TestView = RouteHandlerFunc(func(c *Core) {
+		jsonTest2 := jsonTest{}
+
+		c.Json().DecodeReqBody(&jsonTest2)
+
+		if jsonTest2.Title != "Hello World!" {
+			t.Fail()
+		}
+
+		jsonTest2.Title = "Hola!"
+
+		c.Json().Send(jsonTest2)
+	})
+
+	jsonTest1 := jsonTest{"Hello World!"}
+
+	b, err := json.Marshal(jsonTest1)
+	Check(err)
+
+	ts := httptest.NewServer(App)
+	defer ts.Close()
+
+	req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+	Check(err)
+
+	res, err := http.DefaultClient.Do(req)
+	Check(err)
+
+	err = json.NewDecoder(res.Body).Decode(&jsonTest1)
+	Check(err)
+
+	if jsonTest1.Title != "Hola!" {
+		t.Fail()
+	}
 }
--- xml_test.go
+++ xml_test.go
@@ -1,5 +1,13 @@
 package core
 
+import (
+	"bytes"
+	"encoding/xml"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
 func ExampleCore_Xml(c *Core) {
 	// Prepare structure
 	data := struct {
@@ -12,4 +20,49 @@ func ExampleCore_Xml(c *Core) {
 
 	// Send it back to the client
 	c.Xml().Send(data)
+}
+
+func TestXml(t *testing.T) {
+	type xmlTest struct {
+		Title string `xml:"title"`
+	}
+
+	App := NewApp()
+
+	App.Debug = true
+
+	App.TestView = RouteHandlerFunc(func(c *Core) {
+		xmlTest2 := xmlTest{}
+
+		c.Xml().DecodeReqBody(&xmlTest2)
+
+		if xmlTest2.Title != "Hello World!" {
+			t.Fail()
+		}
+
+		xmlTest2.Title = "Hola!"
+
+		c.Xml().Send(xmlTest2)
+	})
+
+	xmlTest1 := xmlTest{"Hello World!"}
+
+	b, err := xml.Marshal(xmlTest1)
+	Check(err)
+
+	ts := httptest.NewServer(App)
+	defer ts.Close()
+
+	req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+	Check(err)
+
+	res, err := http.DefaultClient.Do(req)
+	Check(err)
+
+	err = xml.NewDecoder(res.Body).Decode(&xmlTest1)
+	Check(err)
+
+	if xmlTest1.Title != "Hola!" {
+		t.Fail()
+	}
 }
